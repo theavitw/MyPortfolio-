dcddfa41f3a2e29e37cad12fe30d8a675e8c1af3
Avit Shah <75948358+theavitw@users.noreply.github.com>
2023-01-22 13:06:52 +0530

    responsiveness improved

--- src/Pages/Python.js
+++ src/Pages/Python.js
@@ -14,38 +14,38 @@ function Python() {
         {/* <img src="https://assets.awwwards.com/awards/submissions/2020/02/5e469a5f90b1c614158410.jpg"/> */}
         <div className="project-card-overlay c1">
           <div className="project-card-text">
+            <h2 className="project-title">Calculator</h2>
+            <p className="project-description">Python Based Calculator </p>
+            <p>PYTHON</p>
+            <a href="https://github.com/theavitw/Text-editor.git"><button className='bg-danger dd'>Github Link</button></a>
+          </div>
+         </div>
+        </div>
+        <div className="project-image-container c2">
+        {/* <img src="https://assets.awwwards.com/awards/submissions/2020/02/5e469a5f90b1c614158410.jpg"/> */}
+        <div className="project-card-overlay ">
+          <div className="project-card-text">
+            <h2 className="project-title">CollegeGuardian</h2>
+            <p className="project-description">FrontEnd Project </p>
+            <p>HTML + CSS + JS</p>
+            <a href="https://github.com/theavitw/Text-editor.git"><button className='bg-danger dd'>Github Link</button></a>
+          </div>
+         </div>
+        </div>
+        <div className="project-image-container c3">
+        {/* <img src="https://assets.awwwards.com/awards/submissions/2020/02/5e469a5f90b1c614158410.jpg"/> */}
+        <div className="project-card-overlay ">
+          <div className="project-card-text">
             <h2 className="project-title">Text-Editor</h2>
             <p className="project-description">Python Based TextEditor Software </p>
             <p>PYTHON</p>
             <a href="https://github.com/theavitw/Text-editor.git"><button className='bg-danger dd'>Github Link</button></a>
           </div>
-         
+         </div>
         </div>
-        <div className="project-card-overlay c2">
-          <div className="project-card-text">
-            <h2 className="project-title">CollegeGuardian</h2>
-            <p className="project-description">WebSite for College Folks</p>
-            <p>HTML+CSS+JS</p>
-            <a href="https://theavitw.github.io/CollageGuardia/index.html"><button className='bg-danger dd'>Website Link</button></a>
-
-          </div>
-         
-        </div>
-        <div className="project-card-overlay c3">
-          <div className="project-card-text">
-            <h2 className="project-title">Calculator </h2>
-            <p className="project-description">Python Based Software </p>
-            <p>PYTHON</p>
-
-            <a href="https://github.com/theavitw/Text-editor.git"><button className='bg-danger dd'>Github Link</button></a>
-
-          </div>
-          
-        </div>
-        
-      </div>
       
     </div>
+    
     <Footer />
     </>
     
