6ff254450ab09b140ee30143efa9695bb58e9ca8
Avit Shah <75948358+theavitw@users.noreply.github.com>
2023-01-23 11:30:34 +0530

    Updated icons

--- src/footer.js
+++ src/footer.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Instagramicon from './Instagramicon'
+import GitIcon from "./GitIcon";
 import './ss.css'
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 const current = new Date();
 const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
 function Footer() {
@@ -8,11 +10,12 @@ function Footer() {
     <div>
       <footer class="footer">
         <div class="social">
-          <i class="fab fa-facebook-f fa-2x"></i>
-          <i class="fab fa-twitter fa-2x"></i>
-          <i class="fab fa-instagram fa-2x"></i>
+          
           <p class="footer2">The Avit's Room &copy; {date}, All Right Reserved</p>
         </div>
+        <a href="https://twitter.com/Shah_Avit" className="icon"><GitIcon/></a>
+        <a href="https://www.linkedin.com/in/avitdesai/" className="icon"><Instagramicon/></a>
+
       </footer>
     </div>
   );
