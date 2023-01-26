e75107f2d631c60413150769546b5941d7410b2e
Avit Shah <75948358+theavitw@users.noreply.github.com>
2023-01-27 00:26:13 +0530

    Updated

--- src/Cards react.js
+++ src/Cards react.js
@@ -1,74 +1,39 @@
-import Card from "react-bootstrap/Card";
-import { Stack } from "react-bootstrap";
+
 // import { AiFillAlert } from "react-icons/ai";
 // import CardStyle from './CardStyle'
+
 import "./ss.css";
-import Login from "./Login";
+// import Login from "./Login";
 function Cards() {
   
   return (
     
     <>
-      <Login className="container" />
-      <Stack direction="horizontal" gap={3} className="container">
-        {[
-          "Dark",
-          // 'Secondary',
-
-          "Light",
-
-          "Dark",
-          "Light",
-        ].map((variant) => (
-          <Card
-            bg={variant.toLowerCase()}
-            key={variant}
-            text={variant.toLowerCase() === "light" ? "dark" : "white"}
-            style={{
-              display: "inline-block",
-              position: "relative",
-            }}
-            className="mb-2"
-          >
-            <Card.Header>Header</Card.Header>
-            <Card.Body>
-              <Card.Title>{variant} Card Title </Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        ))}
-      </Stack>
       <section class="features">
         <div class="container">
           <div class="box-wrapper">
             <div class="box box-1">
               <i class="fas fa-space-shuttle fa-2x"></i>
-              <h1 class="md">HEADING</h1>
+              <h1 class="md">Communicaton </h1>
 
               <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Perspiciatis, repellat.
+              Effective communication is essential for building a strong corporate culture, as it promotes shared goals, efficient information sharing, and trust among team members, leading to better decision making, problem solving, and a positive work environment.
               </p>
             </div>
             <div class="box box2">
               <i class="fas fa-people-arrows  fa-2x"></i>
-              <h1 class="md">HEADING</h1>
+              <h1 class="md">Programming</h1>
 
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-                possimus.
+                The main passion of software develepor is Programming which I have 😁
               </p>
             </div>
             <div class="box box3">
               <i class="fas fa-meteor fa-2x"></i>
-              <h1 class="md">HEADING</h1>
+              <h1 class="md">Team-Work</h1>
 
               <p>
-                Lorem, ipsum dolor. ipsum dolor sit, amet consectetur
-                adipisicing elit.
+                  Teamwork is important in a corporate culture because it allows for the collective effort and collaboration of individuals to achieve common goals, leading to increased productivity and success of the organization. It also promotes a sense of unity and shared responsibility among team members, fostering a positive and supportive work environment.
               </p>
               
             </div>
