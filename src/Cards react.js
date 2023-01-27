f50df59f7b4c648b39522d859c3d894d318d918b
Avit Shah <75948358+theavitw@users.noreply.github.com>
2023-01-28 00:17:12 +0530

    Update Cards react.js

--- src/Cards react.js
+++ src/Cards react.js
@@ -25,56 +25,7 @@ function Cards() {
               <h1 class="md">Programming</h1>
 
               <p>
-                
-// import { AiFillAlert } from "react-icons/ai";
-// import CardStyle from './CardStyle'
-
-import "./ss.css";
-// import Login from "./Login";
-function Cards() {
-  
-  return (
-    
-    <>
-      <section class="features">
-        <div class="container">
-          <div class="box-wrapper">
-            <div class="box box-1">
-              <i class="fas fa-space-shuttle fa-2x"></i>
-              <h1 class="md">Communicaton </h1>
-
-              <p>
-              Effective communication is essential for building a strong corporate culture, as it promotes shared goals, efficient information sharing, and trust among team members, leading to better decision making, problem solving, and a positive work environment.
-              </p>
-            </div>
-            <div class="box box2">
-              <i class="fas fa-people-arrows  fa-2x"></i>
-              <h1 class="md">Programming</h1>
-
-              <p>
               programming is a passion of many software engineers because it is a challenging, creative, and constantly evolving field that allows them to turn their ideas into reality, solve complex problems, and make a positive impact on people's lives. It is a calling that requires constant learning and adaptation, and it is a field that fosters collaboration and community.
-              </p>
-            </div>
-            <div class="box box3">
-              <i class="fas fa-meteor fa-2x"></i>
-              <h1 class="md">Team-Work</h1>
-
-              <p>
-                  Teamwork is important in a corporate culture because it allows for the collective effort and collaboration of individuals to achieve common goals, leading to increased productivity and success of the organization. It also promotes a sense of unity and shared responsibility among team members, fostering a positive and supportive work environment.
-              </p>
-              
-            </div>
-          </div>
-        </div>
-        
-      </section>
-      
-    </>
-  );
-}
-
-export default Cards;
-
               </p>
             </div>
             <div class="box box3">
